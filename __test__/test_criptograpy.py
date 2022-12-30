e953e839aad9c140d4bfc1cb245a6160d67c04ce
мανєη <37839642+EmnlSoc7@users.noreply.github.com>
2022-12-30 14:07:44 -0300

    Encryption List Generated
    Created logical generation of a list with the encrypted text.

--- __test__/test_criptograpy.py
+++ __test__/test_criptograpy.py
@@ -10,14 +10,16 @@ class TestCriptographyEBMethods(unittest.TestCase):
     def test_encrypt(self):
         """Responsável por testar a encriptação"""
 
-        cript_eb = CriptographyEB({"char_a1": ("E", "2"), "char_a2": ("J", "8")})
+        cript_eb = CriptographyEB({"char_a1": ("E", 2), "char_a2": ("J", 8)})
         mensagem_data = cript_eb.encrypt("bem vindo à criptografia de transp", "banana")
+        # b a n a n a
+        # 4 1 5 2 6 3
 
         self.assertEqual(cript_eb.char_a1, "E")
-        self.assertEqual(cript_eb.char_a1_pos, "2")
+        self.assertEqual(cript_eb.char_a1_pos, 2)
 
         self.assertEqual(cript_eb.char_a2, "J")
-        self.assertEqual(cript_eb.char_a2_pos, "8")
+        self.assertEqual(cript_eb.char_a2_pos, 8)
 
         # self.assertTrue((len(mensagem_data) + 2) % 5 == 0)  # Verifica se gerou multiplo de 5 na Mensagem
         # self.assertEqual( messagem_data, "EEOTI AZJVC GDSNI ATZBD PFRZM AOANZ IRREP") Verifica Mensagem final de {"char_a1": ("E", "2"), "char_a2": ("J", "8")} com encriptação de "bem vindo à criptografia de transp", "banana"
@@ -25,14 +27,14 @@ class TestCriptographyEBMethods(unittest.TestCase):
     def test_convert_key(self):
         """Responsável por testar a conversão da chave para numérica"""
 
-        cript_eb = CriptographyEB({"char_a1": ("E", "2"), "char_a2": ("J", "8")})
+        cript_eb = CriptographyEB({"char_a1": ("E", 2), "char_a2": ("J", 8)})
         key = cript_eb.convert_key("banana")
 
         self.assertEqual(cript_eb.char_a1, "E")
-        self.assertEqual(cript_eb.char_a1_pos, "2")
+        self.assertEqual(cript_eb.char_a1_pos, 2)
 
         self.assertEqual(cript_eb.char_a2, "J")
-        self.assertEqual(cript_eb.char_a2_pos, "8")
+        self.assertEqual(cript_eb.char_a2_pos, 8)
 
         self.assertEqual(int(len(key)), int(max(key)))
         self.assertListEqual(key, [4, 1, 5, 2, 6, 3])
--- criptography.py
+++ criptography.py
@@ -10,10 +10,10 @@ class CriptographyEB:
         """Inicializacao da classe"""
 
         self.char_a1 = char_au["char_a1"][0]
-        self.char_a1_pos = char_au["char_a1"][1]
+        self.char_a1_pos = int(char_au["char_a1"][1])
 
         self.char_a2 = char_au["char_a2"][0]
-        self.char_a2_pos = char_au["char_a2"][1]
+        self.char_a2_pos = int(char_au["char_a2"][1])
 
     def convert_key(self, keyword):
         """Converte Chave de Caractere para Ordem Numérica"""
@@ -64,6 +64,35 @@ class CriptographyEB:
         # Criado sem numpy, enumerado os indexes na ordem de criptografia
         key_indexes = [i[0] for i in sorted(enumerate(key), key=lambda x: x[1])]
         print(key_indexes)
+        print(matrix)
+
+        temp_mat2 = []
+        encrypted_message = []
+        count_w = 1
+        for i in key_indexes:
+            print(i)
+            for line in matrix:
+                for head, column in enumerate(line):
+
+                    if len(temp_mat2) == 5:
+                        encrypted_message.append(temp_mat2)
+                        temp_mat2 = []
+
+                    if count_w == self.char_a1_pos:
+                        temp_mat2.append(self.char_a1)
+                        count_w += 1
+                    elif count_w == self.char_a2_pos:
+                        temp_mat2.append(self.char_a2)
+                        count_w += 1
+
+                    if head == i:
+                        if len(temp_mat2) == 5:
+                            encrypted_message.append(temp_mat2)
+                            temp_mat2 = []
+
+                        temp_mat2.append(column)
+                        count_w += 1
+                        print(f"char: {column}")
 
         return matrix
 
@@ -74,7 +103,7 @@ class CriptographyEB:
 
 
 if __name__ == "__main__":
-    cript_eb = CriptographyEB({"char_a1": ("E", "2"), "char_a2": ("J", "8")})
+    cript_eb = CriptographyEB({"char_a1": ("E", 2), "char_a2": ("J", 8)})
     mensagem_data = cript_eb.encrypt("bem vindo à criptografia de transp", "banana")
 
     print(mensagem_data)
