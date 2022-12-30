becc3fa24a832d962ba91c8191a17f531250b952
мανєη <37839642+EmnlSoc7@users.noreply.github.com>
2022-12-29 23:54:42 -0300

    indentation correction
    Adjusted code to Black formatter

--- __test__/test_criptograpy.py
+++ __test__/test_criptograpy.py
@@ -10,38 +10,33 @@ class TestCriptographyEBMethods(unittest.TestCase):
     def test_encrypt(self):
         """Responsável por testar a encriptação"""
 
-        cript_eb = CriptographyEB(
-            {"char_a1": ('E', '2'), "char_a2": ('J', '8')}
-        )
-        mensagem_data = cript_eb.encrypt(
-            'bem vindo à criptografia de transp', 'banana')
+        cript_eb = CriptographyEB({"char_a1": ("E", "2"), "char_a2": ("J", "8")})
+        mensagem_data = cript_eb.encrypt("bem vindo à criptografia de transp", "banana")
 
-        self.assertEqual(cript_eb.char_a1, 'E')
-        self.assertEqual(cript_eb.char_a1_pos, '2')
+        self.assertEqual(cript_eb.char_a1, "E")
+        self.assertEqual(cript_eb.char_a1_pos, "2")
 
-        self.assertEqual(cript_eb.char_a2, 'J')
-        self.assertEqual(cript_eb.char_a2_pos, '8')
+        self.assertEqual(cript_eb.char_a2, "J")
+        self.assertEqual(cript_eb.char_a2_pos, "8")
 
-        self.assertEqual(mensagem_data, 'bemvindoacriptografiadetransp')
-        #self.assertEqual( messagem_data, "EEOTI AZJVC GDSNI ATZBD PFRZM AOANZ IRREP")
+        self.assertEqual(mensagem_data, "bemvindoacriptografiadetransp")
+        # self.assertEqual( messagem_data, "EEOTI AZJVC GDSNI ATZBD PFRZM AOANZ IRREP")
 
     def test_convert_key(self):
         """Responsável por testar a conversão da chave para numérica"""
 
-        cript_eb = CriptographyEB(
-            {"char_a1": ('E', '2'), "char_a2": ('J', '8')}
-        )
-        key = cript_eb.convert_key('banana')
+        cript_eb = CriptographyEB({"char_a1": ("E", "2"), "char_a2": ("J", "8")})
+        key = cript_eb.convert_key("banana")
 
-        self.assertEqual(cript_eb.char_a1, 'E')
-        self.assertEqual(cript_eb.char_a1_pos, '2')
+        self.assertEqual(cript_eb.char_a1, "E")
+        self.assertEqual(cript_eb.char_a1_pos, "2")
 
-        self.assertEqual(cript_eb.char_a2, 'J')
-        self.assertEqual(cript_eb.char_a2_pos, '8')
+        self.assertEqual(cript_eb.char_a2, "J")
+        self.assertEqual(cript_eb.char_a2_pos, "8")
 
         self.assertEqual(int(len(key)), int(max(key)))
         self.assertListEqual(key, [4, 1, 5, 2, 6, 3])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
